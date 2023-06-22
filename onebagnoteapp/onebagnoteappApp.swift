cf9dabf3b6eec941cc172c68a426e8141a0b426a
Selim Ustel <stelselim.devs@gmail.com>
2023-06-22 20:17:52 +0200

    Initial Commit

---
+++ onebagnoteapp/onebagnoteappApp.swift
@@ -0,0 +1,17 @@
+//
+//  onebagnoteappApp.swift
+//  onebagnoteapp
+//
+//  Created by Selim Üstel on 22.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct onebagnoteappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
